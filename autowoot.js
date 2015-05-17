c554be179c08ecf89ff365a8810635f7d9d4e687
dj-monstercat <jordansim189189@gmail.com>
2015-05-17 15:36:04 -0500

    Create autowoot.js

---
+++ autowoot.js
@@ -0,0 +1,39 @@
+(function() {
+    var room = $("#room-name > span").text();
+    var notify = false;
+    if ("Notification" in window) {
+        if (Notification.permission === "granted") {
+            notify = true;
+        } else if (Notification.permission === "default") {
+            Notification.requestPermission(function(permission) {
+                if (!("permission" in Notification && permission === "granted")) {
+                    Notification.permission = permission;
+                    notify = true;
+                }
+            });
+        }
+    }
+
+    API.on(API.ADVANCE, function(data) {
+        if (notify) {
+            var notification = new Notification(room + " > Now playing:", {
+                body: data.media.author + " - " + data.media.title,
+                icon: "http://i.benzi.io/WGXI.png"
+            });
+            setTimeout(function() {
+                notification.close();
+            }, 10000);
+        }
+    });
+
+    API.chatLog("Autowoot turned ON",true);
+    API.chatLog("Credit: http://benzi.io");
+
+    $("#woot").click();
+
+    API.on(API.ADVANCE,function(){
+    	setTimeout(function(){
+    		$("#woot").click();
+    	},1000);
+    });
+}());
