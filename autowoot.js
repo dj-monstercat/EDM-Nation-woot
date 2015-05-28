851f7070909a135c3caa7c83cbf087434ba204a1
dj-monstercat <jordansim189189@gmail.com>
2015-05-28 01:42:42 -0500

    Update autowoot.js

--- autowoot.js
+++ autowoot.js
@@ -1,8 +1,10 @@
 (function() {
                 API.chatLog("error Connecting to EDM Nation servers");
+                
+                
                 }
             });
-        }
+        }     
     }
     
     
